117ab5250d38c07fc1abd5405b09cff9b066deb8
ASHITHA MARY V T <138787878+ashithamaryvt@users.noreply.github.com>
2023-12-20 18:53:17 +0530

    Update delete_task.py

--- functions/delete_task.py
+++ functions/delete_task.py
@@ -1,2 +1,3 @@
-def delete_task(tasks, task_index):
-    print("NA")
+def delete_task(tasks, task_index):        
+    deleted_task = tasks.pop(task_index - 1)
+    print(f"Task '{deleted_task['title']}' deleted successfully.")
